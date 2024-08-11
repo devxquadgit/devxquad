baf2472b93ddd8796ed5da0645264ef3a95519b7
thisisaj1999 <atuljoshi94170@gmail.com>
2024-08-11 12:34:36 +0400

    [Init]: Temp UI

---
+++ assets/js/vendors/parallax.js
@@ -0,0 +1,6 @@
+var scenes = document.querySelectorAll(".scene"),
+    parallaxInstances = [];
+scenes.forEach(function(a) {
+    a = new Parallax(a);
+    parallaxInstances.push(a)
+});
---
+++ assets/js/vendors/scrollcue.js
@@ -0,0 +1,5 @@
+scrollCue.init({
+    interval: -400,
+    duration: 600,
+    percentage: .8
+}), scrollCue.update();
