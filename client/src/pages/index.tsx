a99d8562f5cd3e801dc7671546a4e2fb856b9a33
thisisaj1999 <atuljoshi94170@gmail.com>
2024-08-16 18:02:28 +0400

    [Update]: Testing tag

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import WButton from '../theme/WButton';
+import WTypo from '../theme/WTypo';
+import WTag from '@/theme/WTag';
+
+import TwitterIcon from '../assets/twitter.svg';
+
 
 function index() {
   return (
     <div>
-      <WButton
+      {/* <WButton
         label="Contact us"
         bgcolor="#b80b0a"
         textColor="white"
         width="115px"
         rounded
-        function={() => console.log('Button clicked!')}
-      />
+        onClick={() => console.log('Button clicked!')}
+      /> */}
+      {/* <WTypo label="Small Subheading" type="sub-md" color="red" /> */}
+      <div>
+        <WTag
+          icon={<TwitterIcon width={22} height={22}/>}
+          href="https://twitter.com/example" 
+          type="circle-md"
+          bordered
+          clickable
+        />
+        <WTag
+          icon={<TwitterIcon width={22} height={22}/>}
+          label="Twitter"
+          href="https://twitter.com/example"
+          type="pill"
+          bordered
+        />
+      </div>
     </div>
   );
 }
