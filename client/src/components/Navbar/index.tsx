03eed560824aec1f0ba31239efc40f25c621764d
thisisaj1999 <atuljoshi94170@gmail.com>
2024-08-23 23:03:17 +0400

    [Add]: Menu SVG on small screen

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Logo from '@/assets/logo/logo.svg';
+import Menu from '@/assets/icons/menu.svg';
 import WTypo from '@/theme/WTypo';
 import WButton from '@/theme/WButton';
 import Eyes from '@/components/Eyes'
@@ -14,6 +15,11 @@ const StyledNav = styled.nav`
   border-bottom: 1px solid #f1f5f9;
   background-color: #fff;
   box-shadow: 0 0.125rem 0.25rem rgba(2, 6, 23, 0.075);
+
+  @media (max-width: 900px) {
+    display: none;
+  }
+
 `;
 
 const StyledNavLinks = styled.div`
@@ -24,6 +30,27 @@ const StyledNavLinks = styled.div`
 const LogoWrapper = styled(Logo)`
   width: 80px;
   height: 48px;
+`;
+
+const MenuWrapper = styled(Menu)`
+  width: 30px;
+  height: 30px;
+  color: #1e293b;
+`;
+
+const StyledMenu = styled.div`
+  display: none;
+
+  @media (max-width: 900px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 60px;
+    width: 100%;
+    border-bottom: 1px solid #f1f5f9;
+    background-color: #fff;
+    box-shadow: 0 0.125rem 0.25rem rgba(2, 6, 23, 0.075);
+  }
 `;
 
 const navLinks = [
@@ -38,26 +65,32 @@ const navLinks2 = [
 
 const Index: React.FC = () => {
   return (
-    <StyledNav>
-      <LogoWrapper />
-      <StyledNavLinks>
-        {navLinks.map(({ label, url }) => (
-          <WTypo key={label} label={label} type="link" url={url} color="#64748b" />
-        ))}
-        <Eyes />
-        {navLinks2.map(({ label, url }) => (
-          <WTypo key={label} label={label} type="link" url={url} color="#64748b" />
-        ))}
-      </StyledNavLinks>
-      <WButton
-        label="Contact us"
-        bgcolor="#b80b0a"
-        textColor="white"
-        width="115px"
-        rounded
-        onClick={() => console.log('Button clicked!')}
-      />
-    </StyledNav>
+    <>
+      <StyledNav>
+        <LogoWrapper />
+        <StyledNavLinks>
+          {navLinks.map(({ label, url }) => (
+            <WTypo key={label} label={label} type="link" url={url} color="#64748b" />
+          ))}
+          <Eyes />
+          {navLinks2.map(({ label, url }) => (
+            <WTypo key={label} label={label} type="link" url={url} color="#64748b" />
+          ))}
+        </StyledNavLinks>
+        <WButton
+          label="Contact us"
+          bgcolor="#b80b0a"
+          textColor="white"
+          width="115px"
+          rounded
+          onClick={() => console.log('Button clicked!')}
+        />
+      </StyledNav>
+      <StyledMenu>
+        <LogoWrapper />
+        <MenuWrapper />
+      </StyledMenu>  
+    </>
   );
 };
 
