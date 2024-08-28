13ef77290aaf680b57b7525fa180997300e09861
thisisaj1999 <atuljoshi94170@gmail.com>
2024-08-28 23:04:41 +0400

    [Add]: About section

--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Styled from '@emotion/styled';
+import Feature from '@/layouts/Feature'
+import Info from '@/layouts/Info'
+import Newsletter from '@/layouts/Newsletter';
 
-const index = () => {
+
+const ContainerDiv = Styled.div`
+  --bs-gutter-x: 2.5rem;
+  --bs-gutter-y: 0;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: calc(var(--bs-gutter-x)* .5);
+  padding-right: calc(var(--bs-gutter-x)* .5);
+  width: 100%;
+  
+  @media (min-width: 1400px) {
+    max-width: 1320px;
+  }
+  
+  @media (max-width: 767px) {
+    max-width: 540px;
+  }
+`;
+
+
+function index() {
   return (
-    <div>index</div>
-  )
+    <div>
+
+      {/* <ContainerDiv>
+        <Feature />
+      </ContainerDiv> */}
+      <ContainerDiv>
+        <Info />
+      </ContainerDiv>
+      <ContainerDiv>
+        <Newsletter />
+      </ContainerDiv>
+
+    </div>
+  );
 }
 
-export default index
+export default index;
