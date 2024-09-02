61fae28f9fabc9d93bf38801d62313646f7ec8c8
thisisaj1999 <atuljoshi94170@gmail.com>
2024-09-02 23:22:00 +0400

    [Fix]: Card2 for team

--- client/src/components/Card2/index.tsx
+++ client/src/components/Card2/index.tsx
@@ -6,14 +6,19 @@ import Image from 'next/image';
 import { ICard2Props } from '@/contracts/ICard2Props';
 
 const StyledCardMain = styled.div`
+  margin: 2.5rem 0;
+  padding: 0 24px;
+`;
+
+const StyledCard = styled.div`
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
   display: inline-flex;
   flex-direction: column;
   align-items: center;
   border-radius: .75rem;
-  padding: 32px 24px;
+  padding: 32px 0;
   max-width: 14rem;
-  width: 100%;
+  width: 14rem;
 `;
 
 const StyledAvatar = styled.div`
@@ -29,20 +34,22 @@ const StyledAvatar = styled.div`
 const index: React.FC<ICard2Props> = ({ Img, Name, Desc, style }) => {
   return (
     <StyledCardMain>
-      <StyledAvatar>
-        <Image alt="Remy Sharp" src={Adbhut} width={80}/>
-      </StyledAvatar>
-      <WTypo
-        label={Name}
-        type="sub-md"
-        color="#64748b"
-        style={{ marginBottom: '.2rem', lineHeight: '1.5' }}
-      />
-      <WTypo
-        label={Desc}
-        type="sub-sm"
-        color="#64748b"
-      />
+      <StyledCard>
+        <StyledAvatar>
+          <Image alt="Sample1" src={Adbhut} width={80}/>
+        </StyledAvatar>
+        <WTypo
+          label={Name}
+          type="sub-md"
+          color="#64748b"
+          style={{ marginBottom: '.2rem', lineHeight: '1.5' }}
+        />
+        <WTypo
+          label={Desc}
+          type="sub-sm"
+          color="#64748b"
+        />
+      </StyledCard>
     </StyledCardMain>
   )
 }
