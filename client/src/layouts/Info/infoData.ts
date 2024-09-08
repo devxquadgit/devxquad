1cbf578331c1106bd38edc1eed3a72749cdbd6dd
thisisaj1999 <atuljoshi94170@gmail.com>
2024-09-08 09:10:39 +0400

    [Update]: data for services

--- client/src/layouts/Info/infoData.ts
+++ client/src/layouts/Info/infoData.ts
@@ -45,23 +45,31 @@ const infoData: IInfoProps[] = [
       {
         Image: Img2.src,
         Topic: "STREAMLINE & THRIVE",
-        Heading: "Transform Chaos into Clarity",
-        Desc: "Ready to make your projects less of a jigsaw puzzle and more of a well-oiled machine? Our easy-to-use tools will turn confusion into control, with visuals so clear you’ll think you’re seeing a magic trick. 🎩✨",
-        Url: "www.google.com"
-      },
-      {
-        Image: Img2.src,
-        Topic: "STREAMLINE & THRIVE",
-        Heading: "Transform Chaos into Clarity",
-        Desc: "Ready to make your projects less of a jigsaw puzzle and more of a well-oiled machine? Our easy-to-use tools will turn confusion into control, with visuals so clear you’ll think you’re seeing a magic trick. 🎩✨",
+        Heading: "Website Design",
+        Desc: "We design visually stunning and user- friendly websites that elevate your online presence. We provide client-centric ui/ux that are user-friendly and aesthetic at the same time.",
         Url: "www.google.com"
       },
       {
         Image: Img3.src,
         Topic: "VISION & EXECUTE",
-        Heading: "From Dreams to Reality in No Time",
-        Desc: "Got big dreams? Our tools turn them into reality faster than you can say “goal achieved!” With crystal-clear views and easy management, your project will be cruising toward success. 🌈🚗",
-        Points: ["Visualize your goals effortlessly", "Manage tasks with finesse", "Stay on top of every detail", "Achieve your milestones with ease"],
+        Heading: "Web Development",
+        Desc: "We build reliable, scalable solutions that deliver your vision and exceed your expectations.",
+        Points: ["Website Development", "Front/Back-end Development", "Full-stack App Development", "Quality Assurance"],
+        Url: "www.google.com"
+      },
+      {
+        Image: Img2.src,
+        Topic: "STREAMLINE & THRIVE",
+        Heading: "Ecommerce development",
+        Desc: "We are at the forefront of modern e-commerce development. Which mainly means adding your logo to the Shopify store template we’ve used for the past six years.",
+        Url: "www.google.com"
+      },
+      {
+        Image: Img3.src,
+        Topic: "VISION & EXECUTE",
+        Heading: "Mobile Development",
+        Desc: "Mobile app development is the act or process by which a mobile app is developed for one or more mobile devices.",
+        Points: ["Flutter : Flutter is an open source framework by Google for building beautiful, natively compiled, multi-platform applications.", "React Native : React Native lets you create truly native apps and doesn't compromise your users' experiences."],
         Url: "www.google.com"
       },
     ]
